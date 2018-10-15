0cbb76420b03e571c957ec680d09892ed9439bad
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2018-10-15 16:33:12 +0100

    ENH: Add variance targetting GARCH model
    Add a VT GARCH model
    Note that inference isn't correct

--- arch/univariate/__init__.py
+++ arch/univariate/__init__.py
@@ -2,12 +2,13 @@ from __future__ import absolute_import
 
 from arch.univariate.mean import HARX, ConstantMean, ZeroMean, ARX, arch_model, LS
 from arch.univariate.volatility import (GARCH, ARCH, HARCH, ConstantVariance, EWMAVariance,
-                                        RiskMetrics2006, EGARCH, FixedVariance, MIDASHyperbolic)
+                                        RiskMetrics2006, EGARCH, FixedVariance, MIDASHyperbolic,
+                                        VarianceTargetingGARCH)
 from arch.univariate.distribution import (Distribution, Normal, StudentsT, SkewStudent,
                                           GeneralizedError)
 
 __all__ = ['HARX', 'ConstantMean', 'ZeroMean', 'ARX', 'arch_model', 'LS',
            'GARCH', 'ARCH', 'HARCH', 'ConstantVariance',
-           'EWMAVariance', 'RiskMetrics2006', 'EGARCH',
+           'EWMAVariance', 'RiskMetrics2006', 'EGARCH', 'VarianceTargetingGARCH',
            'Distribution', 'Normal', 'StudentsT', 'SkewStudent', 'GeneralizedError',
            'FixedVariance', 'MIDASHyperbolic']
